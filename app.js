dea9f38c6bdbd2de592cf7551c9717f75379b3d3
knvsriram2000 <knvsriram2000@gmail.com>
2024-07-31 14:00:12 -0400

    Converting html files to pug files for dynamic content

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express=require('express');
+
 const bodyParser=require('body-parser');
+
 const path=require('path');
+
 const app=express();
+
+app.set('view engine','pug');
+app.set('views','views')
 
 const adminData=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
@@ -13,7 +19,8 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes); 
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404',{'pageTitle':'Page Not Found'});
 });
 
 app.listen(3500);
