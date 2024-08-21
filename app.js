9c54c8dcdc7ac7d2ca3ec070d4490d6e0648b465
knvsriram2000 <knvsriram2000@gmail.com>
2024-08-21 19:14:05 -0400

    converting html files to ejs files for dynamic content

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const express=require('express');
 
 const bodyParser=require('body-parser');
 
-const expressHbs=require('express-handlebars');
+// const expressHbs=require('express-handlebars');
 
 const path=require('path');
 
 const app=express();
 
 // app.set('view engine','pug');
-app.engine('hbs',expressHbs.engine({
-    layoutDir:'views/layouts/',
-    defaultLayout:'main-layout',
-    extname:'hbs'
-}))
-app.set('view engine','hbs');
+// app.engine('hbs',expressHbs.engine({
+//     layoutDir:'views/layouts/',
+//     defaultLayout:'main-layout',
+//     extname:'hbs'
+// }))
+// app.set('view engine','hbs');
+app.set('view engine','ejs');
 app.set('views','views')
 
 const adminData=require('./routes/admin');
@@ -28,7 +29,7 @@ app.use(shopRoutes);
 
 app.use((req,res,next)=>{
     // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-    res.status(404).render('404',{'pageTitle':'Page Not Found'});
+    res.status(404).render('404',{'path':'','pageTitle':'Page Not Found'});
 });
 
 app.listen(3500);
