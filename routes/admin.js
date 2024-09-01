e3d1cc1dc0ddd0435cd1ce29a365e5aea240424c
knvsriram2000 <knvsriram2000@gmail.com>
2024-09-01 11:01:58 -0400

    Adding the cart page and dynamic content to the application

--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,24 @@ const path=require('path');
 
 const rootDir=require('../util/path');
 
-const productsController= require('../controllers/products');
+const adminController= require('../controllers/admin');
 
 const router=express.Router();
 
 // /admin/add-product-GET
-router.get('/add-product',productsController.getAddProduct);
+router.get('/add-product',adminController.getAddProduct);
+
+router.get('/products',adminController.getProducts);
 
 // /admin/add-product-POST
-router.post('/add-product',productsController.postAddProduct);
+
+router.post('/add-product',adminController.postAddProduct);
+
+router.get('/edit-product/:productId',adminController.getEditProduct);
+// query parameters can be added by ? by using key value pairs with = between them separted by & , path is determined upto the question mark
+
+router.post('/edit-product',adminController.postEditProduct);
+
+router.post('/delete-product',adminController.postDeleteProduct);
 
 module.exports=router;
